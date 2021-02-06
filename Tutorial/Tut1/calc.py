d205962f10740869f5b0c68b8b715ee401a6cad1
Subhamoy Mahajan <subhamoymahajan@gmail.com>
2021-02-06 04:06:19 -0800

    Uncomment

--- Tutorial/Tut1/calc.py
+++ Tutorial/Tut1/calc.py
@@ -6,42 +6,42 @@ import NPanalysis as NPa
 # get .gro files using "gmx trjconv -pbc whole -sep -f md_1.xtc -s md_1.tpr -o \
 #     GROs/DP.gro"
 # get mass infromation "gmx dump -s md_1.tpr > trj.dump"
-###1.Reading constants
-#print("Step 1: Storing Constants\n")
-#NPa.pickle_constants('constants.dat', sep=' ') # constants is in the current directory.
-#NPa.gmx.gen_index_mol('molndx.pickle')
-#NPa.gmx.pickle_mass(filename='tpr.dump',mass_pickle='mass.pickle')
-#
-##2.Getting connection matrix from .gro files and finding change required to 
-##  make nanoparticles whole.
-#print("Step 2: Calculating Connection Matrix\n")
-#NPa.connMat.gro2connected(inGRO='Whole/DP', time_pickle='time.pickle',  
-#    connected_pickle='connected.pickle', time_fac=4*1E-6, time_shift=0, 
-#    mindist_pickle='mindist.pickle', ndx_pickle='molndx.pickle')
-#NPa.connMat.get_roles(50, connected_pickle='connected.pickle', mol=1, 
-#    time_pickle='time.pickle', outname='PEI_roles.dat',sep=',')
-#NPa.connMat.get_roles2(50, connected_pickle='connected.pickle', mol=1, 
-#    time_pickle='time.pickle', outname='PEI_roles2.dat',sep=',')
-#
-###3.Calculate clusters
-#print("Step 3: Calculating Clusters\n")
-#NPa.cluster.gen_clusters(connected_pickle='connected.pickle', 
-#    cluster_pickle='cluster.pickle')
-##Average size 
-#NPa.cluster.gen_avgsize(50,'avgsize.dat', cluster_pickle='cluster.pickle', 
-#     time_pickle='time.pickle', main_mol=0, sep=',')
-##Average number of NP and charge of NP as a function of size
-#NPa.cluster.gen_ncNP_s(3,'num_charge_NPs_size.dat', main_mol=0, sep=' ', 
-#    cluster_pickle='cluster.pickle')
-#
-###4.Calculate Radius
-###Make nanoparticles whole
-##print("Step 4: Making Nanoparticles Whole and calculating Radii\n")
-#NPa.gmx.make_NPwhole(inGRO='Whole/DP',outGRO='NPwhole/DP', 
-#    cluster_pickle='cluster.pickle', connected_pickle='connected.pickle', 
-#    ndx_pickle='molndx.pickle')
-#
-###Calculate radius at each timestep.
+##1.Reading constants
+print("Step 1: Storing Constants\n")
+NPa.pickle_constants('constants.dat', sep=' ') # constants is in the current directory.
+NPa.gmx.gen_index_mol('molndx.pickle')
+NPa.gmx.pickle_mass(filename='tpr.dump',mass_pickle='mass.pickle')
+
+#2.Getting connection matrix from .gro files and finding change required to 
+#  make nanoparticles whole.
+print("Step 2: Calculating Connection Matrix\n")
+NPa.connMat.gro2connected(inGRO='Whole/DP', time_pickle='time.pickle',  
+    connected_pickle='connected.pickle', time_fac=4*1E-6, time_shift=0, 
+    mindist_pickle='mindist.pickle', ndx_pickle='molndx.pickle')
+NPa.connMat.get_roles(50, connected_pickle='connected.pickle', mol=1, 
+    time_pickle='time.pickle', outname='PEI_roles.dat',sep=',')
+NPa.connMat.get_roles2(50, connected_pickle='connected.pickle', mol=1, 
+    time_pickle='time.pickle', outname='PEI_roles2.dat',sep=',')
+
+##3.Calculate clusters
+print("Step 3: Calculating Clusters\n")
+NPa.cluster.gen_clusters(connected_pickle='connected.pickle', 
+    cluster_pickle='cluster.pickle')
+#Average size 
+NPa.cluster.gen_avgsize(50,'avgsize.dat', cluster_pickle='cluster.pickle', 
+     time_pickle='time.pickle', main_mol=0, sep=',')
+#Average number of NP and charge of NP as a function of size
+NPa.cluster.gen_ncNP_s(3,'num_charge_NPs_size.dat', main_mol=0, sep=' ', 
+    cluster_pickle='cluster.pickle')
+
+##4.Calculate Radius
+##Make nanoparticles whole
+#print("Step 4: Making Nanoparticles Whole and calculating Radii\n")
+NPa.gmx.make_NPwhole(inGRO='Whole/DP',outGRO='NPwhole/DP', 
+    cluster_pickle='cluster.pickle', connected_pickle='connected.pickle', 
+    ndx_pickle='molndx.pickle')
+
+##Calculate radius at each timestep.
 NPa.radius.calc_Rh_Rg(Rh_pickle='Rh.pickle', Rg2_pickle='Rg2.pickle', sep=',', 
     inGRO='NPwhole/DP', main_mol=0, mass_pickle='mass.pickle', 
     cluster_pickle='cluster.pickle', ndx_pickle='molndx.pickle')
