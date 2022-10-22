3b72712c4040fa0a5b4bd933316f16d35a7f4802
subhamoymahajan <subhamoymahajan@gmail.com>
2022-10-22 10:58:08 -0700

    Updated geometry comments and tests

--- NPanalysis/geometry.py
+++ NPanalysis/geometry.py
@@ -40,7 +40,7 @@ def distance(pos_i,pos_j):
     
     Returns
     -------
-    d2: float
+    d: float
         Distance between two atoms
     """
     return np.sqrt(distance2(pos_i,pos_j))
@@ -52,11 +52,8 @@ def distance2(pos_i,pos_j):
     
     Parameters:
     ----------
-    pos_i: numpy array of floats
-        Position coordinates of atom i
-    pos_j: numpy array of floats
-        Position coordinates of atom j
-    
+    See distance()
+ 
     Returns
     -------
     d2: float
@@ -67,11 +64,27 @@ def distance2(pos_i,pos_j):
 @njit(parallel=True)
 def dist_matrix(pos,maxd=None):
     """Calculates distance matrix for positions
+
+    Parameters
+    ----------
+    pos: 2D numpy array of floats
+        See pos_i or pos_j in distance()
+    maxd: float
+        If square distances are above maxd^2, distance is set as maxd. This is 
+        to avoid costly square root evaluations. Default is None, which 
+        calculates every square root.
+
+    Returns
+    -------
+    dist_mat: 2D numpy array of floats
+        Distances are calculated for j>i. Distances are cutoff at maxd.
     """
     N=len(pos)
     dist_mat=np.zeros((N,N))
     if maxd!=None:
         maxd2=maxd*maxd
+    else:
+        maxd2=np.inf
     for i in prange(N-1):
         for j in prange(i+1,N):
             foo=np.sum(np.square(pos[i]-pos[j]))
@@ -82,8 +95,6 @@ def dist_matrix(pos,maxd=None):
             else:
                 dist_mat[i,j]=np.sqrt(foo)
     return dist_mat       
-            
-   
 
 @njit(parallel=True)
 def get_cen(pos, atoms):
@@ -93,10 +104,10 @@ def get_cen(pos, atoms):
     Parameters
     ----------
     pos: 2D numpy array of floats
-       Contains position of all atoms in the system. Axis 0 is the global atom
-       ID, and axis 1 is the direction.
+        See pos_i or pos_j in distance()
     atoms: numpy array of integers
-       numpy array of global IDs of atoms.
+        numpy array of global IDs of atoms.
+
     Returns
     -------
     cen: numpy array of floats
@@ -107,19 +118,27 @@ def get_cen(pos, atoms):
     return cen  
 
 @njit(parallel=True)  
-def get_pbc_mindist(dna_pos,pei_pos,box,ndna,npei,adna,apei, pbc): 
+def get_pbc_mindist(pos_i,pos_j,box,npos_i,npos_j,apos_i,apos_j, pbc): 
     """Calculate the amount of boxlengths (in x, y, and z direction) required 
-       to move group of atoms (atoms2) to minimize distance between groups of
-       atoms (atoms2 and atoms1).
+       to move group of molecules in (pos_j) to minimize distance between groups
+       of molecules present in (pos_i and pos_j).
 
     Parameters
     ----------
+    pos_i: 2D numpy array of floats
+       see distance()
+    pos_j: 2D numpy array of floats
+       see distance()
     box: numpy array of floats
        Dimensions of cuboidal simulation box in x, y and z. 
-    pos1: 2D array of floats
-       Positions of a group of atoms (1)
-    pos2: list of int
-       Positions of a group of atoms (2)
+    npos_i: int
+       Number of molecules in pos_i
+    npos_j: int
+       Number of molecules in pos_k
+    apos_i: int
+       Number of particles (or atoms) per molecule in pos_i
+    apos_j: int
+       Number of particles (or atoms) per molecule in pos_j
     pbc: list of integer
        List specifying if periodic boundary condition (PBC) is applied in 
        x, y, and z directions.
@@ -127,50 +146,48 @@ def get_pbc_mindist(dna_pos,pei_pos,box,ndna,npei,adna,apei, pbc):
     Returns
     -------
     dmin: 2D numpy array of float
-        Minimum distance considering PBC. Axis 0 and 1 represent DNA and PEI ID
+        Minimum distance considering PBC. Axis 0 and 1 represent pos_i and 
+        pos_j molecules IDs.
     mol_disp: 2D numpy array of float 
-        Displacement of DNAs needed to minimize distance with PEIs. 
-        Axis 0 and 1 represents DNA ID and PEI ID respectively.
+        Displacement of pos_i needed to minimize distance with pos_j. 
+        Axis 0 and 1 represents pos_i and pos_j molecule IDs respectively.
     """
     # Definitions:
-    # change: The amount of box lengths in x, y, and z directions required to 
-    #         move DNA molecules to minimize distance between DNA and PEI 
-    #         molecules
     # PBC distance: distance calculated by apply periodic boundary condition 
     #               based on the pbc variable. pbc[k] = 1 implies pbc is active
     #               in direction k, 0 implies it is inactive. k = 0, 1, 2 
     #               implies directions x, y, and z respectively.
-    N1=len(dna_pos)
-    N2=len(pei_pos)
-    # Position of DNA atoms relative to PEI atoms
-    dr=np.zeros((N1,N2,3))
-    dr2=np.zeros((N1,N2,3))
-    #displacement of DNA needed to minimize distance with PEI
+    N1=len(pos_i)
+    N2=len(pos_j)
+    # Position of pos_j atoms relative to pos_i atoms
+    dr=np.zeros((N1,N2,3)) #displacement pos_i-pos_j
+    dr2=np.zeros((N1,N2,3)) #displacement pos_i-pos_j after PBC
+    
     #disp=np.zeros((N1,N2,3)) 
     for i in prange(N1):
         for j in prange(N2):
             for k in range(3):
-                dr[i,j,k]=dna_pos[i][k]-pei_pos[j][k] #relative position
-    
+                dr[i,j,k]=pos_i[i][k]-pos_j[j][k] #relative position
     for k in range(3):
         if pbc[k]==1:#Apply PBC 
             dr2[:,:,k]=np.mod(dr[:,:,k]+0.5*box[k],box[k])-0.5*box[k] 
         else:
             dr2[:,:,k]=dr[:,:,k]
-    # Displacement of DNA atoms needed to minimize distance with PEI atoms
-    disp=dr2-dr 
+    # Displacement of move pos_j atoms to minimize distance with pos_i atoms
+    disp=dr2-dr
     d2=np.sum(np.square(dr2),axis=2) #square distance with PBC 
-    dmin2=np.zeros((ndna,npei)) #Intialize minimum distance between DNA-PEI
-    # Displacement of DNA molecules needed to minimize distance with PEI.
-    mol_disp=np.zeros((ndna,npei,3)) 
-    N=np.zeros((ndna,npei),dtype=int64)
-    for d in prange(ndna):
-        for p in prange(npei):
-            N[d,p]=np.argmin(d2[d*adna:(d+1)*adna,p*apei:(p+1)*apei])
-            dmin2[d,p]=d2[d*adna+int(N[d,p]/apei),p*apei+np.mod(N[d,p],apei)]
-            mol_disp[d,p,:]=disp[d*adna+int(N[d,p]/apei), \
-                p*apei+np.mod(N[d,p],apei),:]
-    dmin=np.sqrt(dmin2) #taking sqrt outside the for loop reduces computation.
+    dmin2=np.zeros((npos_i,npos_j)) #Intialize minimum distance 
+    # Displacement of pos_j molecules needed to minimize distance with 
+    # pos_i molecules.
+    mol_disp=np.zeros((npos_i,npos_j,3)) 
+    N=np.zeros((npos_i,npos_j),dtype=int64)
+    for d in prange(npos_i):
+        for p in prange(npos_j):
+            N[d,p]=np.argmin(d2[d*apos_i:(d+1)*apos_i,p*apos_j:(p+1)*apos_j])
+            dmin2[d,p]=d2[d*apos_i+int(N[d,p]/apos_j),p*apos_j+np.mod(N[d,p],apos_j)]
+            mol_disp[d,p,:]=disp[d*apos_i+int(N[d,p]/apos_j), \
+                p*apos_j+np.mod(N[d,p],apos_j),:]
+    dmin=np.sqrt(dmin2) 
     return dmin,mol_disp
 
 @njit(parallel=True)
@@ -178,26 +195,26 @@ def get_pbcdisp_mols(pos, box, atoms1, atoms2, pbc): #@
     """Calculate the displacement required to move atoms2 to minimize distance
         between groups of atoms2 and atoms1
 
+    While get_pbc_mindist() can be used to calculated the required displacement,
+    get_pbcdisp_mols is faster because square roots are not performed.
     Parameters
     ----------
     pos: 2D numpy array of floats
-       Contains position of all atoms in the system. Axis 0 is the global atom
-       ID, and axis 1 is the direction.
+       See pos_i or pos_j in distance()
     box: numpy array of floats
-       Dimensions of cuboidal simulation box in x, y and z. 
+       See get_pbc_mindist()
     atoms1: list of int
-       Global ID of atoms in a group (1)
+       Global ID of atoms in a group (1). 
     atoms2: list of int
        Global ID of atoms in a group (2)
     pbc: list of integer
-       List specifying if periodic boundary condition (PBC) is applied in 
-       x, y, and z directions.
+       See get_pbc_mindist()
 
     Returns
     -------
     min_disp: array of ints
-        Contains the displacement of atoms1 needed to minimize its distance with
-         atoms2 
+        Contains the displacement of atoms2 needed to minimize its distance with
+        atoms1. Similar to mol_disp in get_pbc_mindist() 
     """
     # Definitions:
     # change: The amount of box lengths in x, y, and z directions required to 
@@ -206,7 +223,7 @@ def get_pbcdisp_mols(pos, box, atoms1, atoms2, pbc): #@
     # PBC distance: distance calculated by apply periodic boundary condition 
     #               based on the pbc variable. pbc[k] = 1 implies pbc is active
     #               in direction k, 0 implies it is inactive. k = 0, 1, 2 
-    #               implies directions x, y, and z respectively. 
+    #               implies directions x, y, and z respectively.
     dr=np.zeros((len(atoms1),len(atoms2),3))
     dr2=np.zeros((len(atoms1),len(atoms2),3))
     for i in prange(len(atoms1)):
@@ -221,7 +238,7 @@ def get_pbcdisp_mols(pos, box, atoms1, atoms2, pbc): #@
         else:
             dr2[:,:,k]=dr[:,:,k]
     disp=dr2-dr
-    d2=np.sum(np.square(disp),axis=2)
+    d2=np.sum(np.square(dr2),axis=2)
     N=np.argmin(d2)
     i=int(N/len(d2[0]))
     j=N-len(d2[0])*i
@@ -235,13 +252,12 @@ def change_pos(pos,disp,atoms):
     Parameters
     ----------
     pos: 2D numpy array of floats
-       Contains position of all atoms in the system. Axis 0 is the global atom
-       ID, and axis 1 is the direction.
+       See dist_matrix()
     disp: numpy array of integers
-       Displacement to be applied to atoms.
+       See min_disp in get_pbcdisp_mols()
     atoms: numpy array of integers
-       List of global IDs of atoms. 
-  
+       See atoms in get_cen()
+ 
     Returns
     -------
     pos: 2d numpy array of floats
@@ -260,15 +276,13 @@ def change_NPpos(pos, box, atoms, pbc):
     Parameters
     ----------
     pos: 2D numpy array of floats
-       Contains position of all atoms in the system. Axis 0 is the global atom
-       ID, and axis 1 is the direction.
+       See pos_i or pos_j in distance()
     box: numpy array of floats
-       Dimensions of cuboidal simulation box in x, y and z. 
+       See box in get_pbc_mindist()
     atoms: List of integers
-       List of global IDs of atoms
+       See atoms in get_cen()
     pbc: list of integer
-       List specifying if periodic boundary condition (PBC) is applied in 
-       x, y, and z directions.
+       See get_pbc_mindist()
 
     Returns
     -------
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -8,6 +8,12 @@ class TestGeometry(unittest.TestCase):
 
     def setUp(self):
         self.pos=np.array([[1,1,1],[2,2,2],[-1,2,-1],[-2,-2,3],[0,0,0]])
+        self.pos1=np.array([[0.2,1.,0.],[0.2,1.2,0.],[0.2,1.2,0.4],
+                            [1.9,1.,0.4],[1.7,1.1,0.5],[1.7,1.2,0.6],
+                            [1.6,1.6,1.6],[1.5,1.4,1.3],[1.4,1.5,1.2]])
+        self.pos2=np.array([[0.2,0.4,0],[0.2,0.4,0.2],[0.2,0.4,0.3],
+                            [1.9,1.,0.],[1.7,1.2,0.],[1.7,1.2,0.2],
+                            [1.2,1.2,1.2],[1.5,1.2,1.2],[1.3,1.4,1.4]])
 
     def test_distance2(self):
         self.assertAlmostEqual(geo.distance2(self.pos[0],self.pos[-1]),3)
@@ -17,6 +23,69 @@ class TestGeometry(unittest.TestCase):
         self.assertAlmostEqual(geo.distance(self.pos[1],self.pos[2]),np.sqrt(18))
         self.assertAlmostEqual(geo.distance(self.pos[3],self.pos[0]),np.sqrt(22))
 
+    def test_distmat(self):
+        mat=geo.dist_matrix(self.pos,maxd=None)
+        self.assertAlmostEqual(mat[0,1],np.sqrt(3))
+        self.assertAlmostEqual(mat[0,2],3)
+        self.assertAlmostEqual(mat[0,3],np.sqrt(22))
+        self.assertAlmostEqual(mat[0,4],np.sqrt(3))
+        self.assertAlmostEqual(mat[1,2],np.sqrt(18))
+        self.assertAlmostEqual(mat[1,3],np.sqrt(33))
+        self.assertAlmostEqual(mat[1,4],np.sqrt(12))
+        self.assertAlmostEqual(mat[2,3],np.sqrt(33))
+        self.assertAlmostEqual(mat[2,4],np.sqrt(6))
+        self.assertAlmostEqual(mat[3,4],np.sqrt(17))
 
+    def test_distmat2(self):
+        mat=geo.dist_matrix(self.pos,maxd=4.)
+        self.assertAlmostEqual(mat[0,1],np.sqrt(3))
+        self.assertAlmostEqual(mat[0,2],3.)
+        self.assertAlmostEqual(mat[0,3],4.)
+        self.assertAlmostEqual(mat[0,4],np.sqrt(3))
+        self.assertAlmostEqual(mat[1,2],4.)
+        self.assertAlmostEqual(mat[1,3],4.)
+        self.assertAlmostEqual(mat[1,4],np.sqrt(12))
+        self.assertAlmostEqual(mat[2,3],4.)
+        self.assertAlmostEqual(mat[2,4],np.sqrt(6))
+        self.assertAlmostEqual(mat[3,4],4.)
+
+    def test_cen(self):
+        cen=geo.get_cen(self.pos,np.array([0,1,2]))
+        self.assertAlmostEqual(cen[0],2./3.)
+        self.assertAlmostEqual(cen[1],5./3.)
+        self.assertAlmostEqual(cen[2],2./3.)
+
+    def test_pbcmindist(self):
+        dmin,mol_disp=geo.get_pbc_mindist(self.pos1,self.pos2,np.array([2.,2.,2.]),3,3,3,3,[1,1,1])
+        dmin_act=[[0.6,0.3,np.sqrt(1.13)],
+                  [np.sqrt(0.46),np.sqrt(0.1),np.sqrt(0.4)],
+                  [np.sqrt(1.16),np.sqrt(0.33),np.sqrt(0.05)]
+                 ]
+        disp_act=[ [[0.,0.,0.],[2.,0.,0.],[2.,0.,0.]],
+                   [[-2.,0.,0.],[0.,0.,0.],[0.,0.,0.]],
+                   [[-2.,-2.,-2.],[0.,0.,-2.],[0.,0.,0.]]]
+        for j in range(3):
+            for i in range(3):
+                self.assertAlmostEqual(dmin[i,j],dmin_act[i][j]) 
+                for k in range(3):
+                    if k==0 and i==2: #Two possible mindist with different mol_disp
+                        continue
+                    self.assertAlmostEqual(mol_disp[k,i,j],disp_act[k][i][j]) 
+
+    def test_pbcdisp(self):
+        disp_act=[ [[0.,0.,0.],[2.,0.,0.],[2.,0.,0.]],
+                   [[-2.,0.,0.],[0.,0.,0.],[0.,0.,0.]],
+                   [[-2.,-2.,-2.],[0.,0.,-2.],[0.,0.,0.]]]
+        pos=np.concatenate((self.pos1,self.pos2))
+        for i in range(3):
+            for j in range(3):
+                if i==0 and j==2: #Two possible mindist with different mol_disp
+                    continue
+                disp=geo.get_pbcdisp_mols(pos,np.array([2.,2.,2.]),
+                    np.arange(i*3,i*3+3),np.arange(j*3+9,j*3+12),[1,1,1])
+                for k in range(3):
+                    self.assertAlmostEqual(-disp[k],disp_act[i][j][k])
+
+    # Tests are not performed for change_NPpos(), change_pos().
 if __name__ == '__main__':
     unittest.main()
