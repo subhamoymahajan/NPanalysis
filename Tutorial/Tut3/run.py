1eec99bb068482866935e167b9c68db53ac1bb82
subhamoymahajan <subhamoymahajan@gmail.com>
2022-10-22 14:30:20 -0700

    optimized scripts and removed comments

--- Tutorial/Tut3/run.py
+++ Tutorial/Tut3/run.py
@@ -1,15 +1,15 @@
 import NPanalysis as NPa
 
-#NPa.cluster.gen_GPT(GPT_in_pickle=None, 
-#    UniqNSGs_in_pickle = None, 
-#    main_mol=0, #DNA
-#    GPT_out_pickle='GPT.pickle', #Output
-#    UniqNSGs_out_pickle = 'UniqNSGs.pickle', #Output 
-#    connected_pickle='connected.pickle', #Input
-#    time_pickle = 'time.pickle', #Input 
-#    log=True,
-#    logfile='GPT.log', #Output
-#)
+NPa.cluster.gen_GPT(GPT_in_pickle=None, 
+    UniqNSGs_in_pickle = None, 
+    main_mol=0, #DNA
+    GPT_out_pickle='GPT.pickle', #Output
+    UniqNSGs_out_pickle = 'UniqNSGs.pickle', #Output 
+    connected_pickle='connected.pickle', #Input
+    time_pickle = 'time.pickle', #Input 
+    log=True,
+    logfile='GPT.log', #Output
+)
 
 
 NPa.cluster.plot_GPT(GPT_pickle='GPT.pickle', 
--- Tutorial/Tut3/run2.py
+++ Tutorial/Tut3/run2.py
@@ -1,37 +1,26 @@
 import NPanalysis as NPa
 
-#NPa.cluster.get_weighted_NSGs(wNSG_pickle='wNSG.pickle',  #Output
-#                              GPT_pickle='GPT.pickle', #Input
-#                              UniqNSGs_pickle='UniqNSGs.pickle', #Input
-#                              time_pickle='time.pickle', #Input
-#                              connected_pickle='connected.pickle', #Input 
-#                              main_mol=0, 
-#)
-#
+NPa.cluster.get_weighted_NSGs(wNSG_pickle='wNSG.pickle',  #Output
+                              GPT_pickle='GPT.pickle', #Input
+                              UniqNSGs_pickle='UniqNSGs.pickle', #Input
+                              time_pickle='time.pickle', #Input
+                              connected_pickle='connected.pickle', #Input 
+                              main_mol=0, 
+)
 
-#NPa.cluster.plot_GPT_NSG(0, 
-#                         GPT_pickle='GPT.pickle', #Input
-#                         wNSG_pickle='wNSG.pickle', #Input
-#                         time_pickle='time.pickle', #Input
-#                         NSG_pos_pickle='NSG_pos.pickle', #Output in this case.
-#                         node_size=600,
-#                         labels=True, #Will help interactively set the positions
-#                         time_scale=1,
-#                         rnd_off=4,
-#                         ref_UIDs=None,
-#)
-#
-#NPa.cluster.plot_GPT_NSG(1, 
-#                         GPT_pickle='GPT.pickle', #Input
-#                         wNSG_pickle='wNSG.pickle', #Input
-#                         time_pickle='time.pickle', #Input
-#                         NSG_pos_pickle='NSG_pos.pickle', #Output in this case.
-#                         node_size=600,
-#                         labels=True, #Will help interactively set the positions
-#                         time_scale=1,
-#                         rnd_off=4,
-#                         ref_UIDs=None,
-#)
+refIDs=[0,1]
+for UID in refIDs:
+    NPa.cluster.plot_GPT_NSG(UID, 
+                             GPT_pickle='GPT.pickle', #Input
+                             wNSG_pickle='wNSG.pickle', #Input
+                             time_pickle='time.pickle', #Input
+                             NSG_pos_pickle='NSG_pos.pickle', #Output in this case.
+                             node_size=600,
+                             labels=True, #Will help interactively set the positions
+                             time_scale=1,
+                             rnd_off=4,
+                             ref_UIDs=None,
+    )
 
 NPa.cluster.plot_GPT_NSG_all(GPT_pickle='GPT.pickle', #Input
                          wNSG_pickle='wNSG.pickle', #Input
@@ -45,6 +34,6 @@ NPa.cluster.plot_GPT_NSG_all(GPT_pickle='GPT.pickle', #Input
                          figname='GPT-NSG',
                          dpi=200,
                          arrow=True,
-                         ref_UIDs=[0,1], #Determined in previous steps
+                         ref_UIDs=refIDs, #Determined in previous steps
 )
 
